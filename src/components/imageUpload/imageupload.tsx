91b21ac9d2167f84bd49f50c11ef67b234703a66
Atul Silori <atul.silori@gmail.com>
2021-11-10 16:27:32 +0530

    added bulk upload to imageupload

--- src/components/imageUpload/imageupload.tsx
+++ src/components/imageUpload/imageupload.tsx
@@ -4,6 +4,7 @@
  */
 
 import React, { useState } from 'react';
+import _ from 'lodash';
 import axios from 'axios';
 interface Iresponse {
 	data: {
@@ -12,39 +13,55 @@ interface Iresponse {
 	};
 }
 
+interface IApiData {
+	uri: string;
+	public_id: string;
+	secure_url: string;
+	image_title: string;
+	image_alt_text: string;
+	height: number;
+	width: number;
+	created_at:Date;
+}
+
 const ImageUpload = ()=>{
-  const [dimention, setDimention] = useState({width: 0, height:0});
+  const [filedataForApi, setFileDataForApi] = useState([null])
 
   const saveToApi = async (res: any) => {
-    const uri = 'http://localhost:8001/api/v1/preset/send';
+		const uri = 'http://localhost:8001/api/v1/preset/update';
+		const responseArray: IApiData[] = []; // array of object from cloudinary response
 
-    const public_id = res.data.public_id;
-    const secure_url = res.data.secure_url;
-    const image_title = res.data.original_filename;
-    const image_alt_text = res.data.original_filename;
-    const height = dimention.height;
-    const width = dimention.width;
-    const created_at = res.data.created_at;
+		_.forEach(res, (val) => {
+			let public_id = val.data.public_id;
+			let secure_url = val.data.secure_url;
+			let image_title = val.data.original_filename;
+			let image_alt_text = val.data.original_filename;
+			let height = val.data.height;
+			let width = val.data.width;
+			let created_at = val.data.created_at;
 
-    const data = {
-      uri: uri,
-      public_id: public_id,
-      secure_url: secure_url,
-      image_title: image_title,
-      image_alt_text: image_alt_text,
-      height: height,
-      width: width,
-      created_at: created_at,
-	  };
+			let data = {
+				uri: uri,
+				public_id: public_id,
+				secure_url: secure_url,
+				image_title: image_title,
+				image_alt_text: image_alt_text,
+				height: height,
+				width: width,
+				created_at: created_at,
+			};
+			responseArray.push(data);
+		});
 
-    const response = await axios.post(uri, data);
-    console.log(response);
+		const response = await axios.post(uri, responseArray);
+		console.log(response);
   };
 
-  const uploadToCloudinary = async (uri: string, data: any) => {
-    const res = await axios.post(uri, data);
-    console.log(res);
-    saveToApi(res);
+  const uploadToCloudinary = (uri: string, data: any) => {
+    return axios.post(uri, data).then((response)=>{
+      console.log(response)
+      setFileDataForApi([...filedataForApi, response.data.data]);
+    });
   };
 
   const uploadToApi = async (uri: string, data: any, File: any) => {
@@ -54,33 +71,39 @@ const ImageUpload = ()=>{
       },
     });
     //console.log(res);
-    const form = new FormData();
-    form.append('file', File);
-    form.append('upload_preset', res.data.preset);
-    form.append('cloud_name', res.data.cloud_name);
-    form.append('folder', data);
-    uploadToCloudinary(`https://api.cloudinary.com/v1_1/${res.data.cloud_name}/image/upload`, form);
+
+    // bulk uploading to cloudinary
+    const allUpload =
+       _.forEach(File, (val) => {
+        let form = new FormData();
+        form.append('file', val);
+        form.append('upload_preset', res.data.preset);
+        form.append('cloud_name', res.data.cloud_name);
+        form.append('folder', data);
+        return uploadToCloudinary(`https://api.cloudinary.com/v1_1/${res.data.cloud_name}/image/upload`, form);
+      });
+
+    // after action when all files uploaded to cloudinary
+    axios.all(allUpload).then(() => {
+        saveToApi(filedataForApi);
+	  });
   };
 
   const uploadImage = (e: any): void => {
     if (e.target.files[0] && e.target.files.length) {
-      const reader = new FileReader();
-      reader.onload = function () {
-        var image = new Image();
+      const file_data:File[] = [];
+      // adding all files to file_data array
+      _.range(0, e.target.files.length).forEach((current, index, range)=>{
+        console.log(current, range);
+        let file = e.target.files[index];
+        file_data.push(file);
+      });
 
-        image.src = e.target.result;
-
-        image.onload = function () {
-          //alert(image.width);
-          setDimention({width: image.width, height: image.height});
-        };
-      };
-      reader.readAsDataURL(e.target.files[0]);
       const data = 'string';
       //data.append('user', 'test_practice')
       // data.append('file', e.target.files[0]);
       const uri = 'http://localhost:8001/api/v1/preset/send';
-      uploadToApi(uri, data, e.target.files[0]);
+      uploadToApi(uri, data, file_data);
     }
   };
   return <input type='file' accept='images/*' onChange={uploadImage} style={{ margin: '10em 10em' }} />;
